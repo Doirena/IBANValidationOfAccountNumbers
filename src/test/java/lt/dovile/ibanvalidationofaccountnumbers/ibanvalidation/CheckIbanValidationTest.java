ee062ef1edbeaa26ec796c182dff1f14a225d3e5
Doreina <barkauskaite.dovile@gmail.com>
2020-07-20 02:28:31 +0300

    correct commits in unitTest

--- src/test/java/lt/dovile/ibanvalidationofaccountnumbers/ibanvalidation/CheckIbanValidationTest.java
+++ src/test/java/lt/dovile/ibanvalidationofaccountnumbers/ibanvalidation/CheckIbanValidationTest.java
@@ -15,12 +15,18 @@ import org.junit.Test;
  */
 public class CheckIbanValidationTest {
 
+    /**
+     * Test of isValid method if its true
+     */
     @Test
     public void test_iban_isValid_true() {
         CheckIbanValidation c = new CheckIbanValidation();
         assertTrue(c.isValid(new Iban("LT647044001231465456")));
     }
-    
+
+    /**
+     * Test of isValid method if its false
+     */
     @Test
     public void test_iban_isValid_false() {
         CheckIbanValidation c = new CheckIbanValidation();
