487136f6268071285d271827b4be3586c3064316
Doreina <barkauskaite.dovile@gmail.com>
2020-07-19 22:33:42 +0300

    two junit tests for isValid() method

---
+++ src/test/java/lt/dovile/ibanvalidationofaccountnumbers/ibanvalidation/CheckIbanValidationTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lt.dovile.ibanvalidationofaccountnumbers.ibanvalidation;
+
+import lt.dovile.ibanvalidationofaccountnumbers.data.Iban;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author Dovile Barkauskaite <barkauskaite.dovile@gmail.com>
+ */
+public class CheckIbanValidationTest {
+
+    @Test
+    public void test_iban_isValid_true() {
+        CheckIbanValidation c = new CheckIbanValidation();
+        assertTrue(c.isValid(new Iban("LT647044001231465456")));
+    }
+    
+    @Test
+    public void test_iban_isValid_false() {
+        CheckIbanValidation c = new CheckIbanValidation();
+        assertFalse(c.isValid(new Iban("CC051245445454552117989")));
+    }
+}
