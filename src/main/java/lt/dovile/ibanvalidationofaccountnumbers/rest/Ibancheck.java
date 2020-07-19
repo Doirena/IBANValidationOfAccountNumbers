835164e1eebd2117dbb129764194ad8a666c2b05
Doreina <barkauskaite.dovile@gmail.com>
2020-07-19 22:03:03 +0300

    add post request for list of Iban

--- src/main/java/lt/dovile/ibanvalidationofaccountnumbers/rest/Ibancheck.java
+++ src/main/java/lt/dovile/ibanvalidationofaccountnumbers/rest/Ibancheck.java
@@ -5,12 +5,16 @@
  */
 package lt.dovile.ibanvalidationofaccountnumbers.rest;
 
-
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import lt.dovile.ibanvalidationofaccountnumbers.data.Iban;
+import lt.dovile.ibanvalidationofaccountnumbers.ibanvalidation.CheckIbanValidation;
 
 /**
  *
@@ -18,20 +22,36 @@ import lt.dovile.ibanvalidationofaccountnumbers.data.Iban;
  */
 @Path("/ibanValidation")
 public class Ibancheck {
-    
+
     /**
-     * 
+     *
      * @return iban
      */
-
     @GET
     @Path("/get")
     @Produces(MediaType.APPLICATION_JSON)
     public Iban getTrackInJSON() {
         Iban iban = new Iban("LT647044001231465456");
         return iban;
-
     }
-   
+
+    /**
+     *
+     * @param ibanList
+     * @return new ibanList which numbers is correct(true)
+     */
+    @POST
+    @Path("/post")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Iban> validIbanList(List<Iban> ibanList) {
+        List<Iban> newIbanList = new ArrayList();
+        for (int i = 0; i < ibanList.size(); i++) {
+            if (new CheckIbanValidation().isValid(ibanList.get(i))) {
+                newIbanList.add(ibanList.get(i));
+            }
+        }
+        return newIbanList;
+    }
 
 }
